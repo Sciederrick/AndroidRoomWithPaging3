a0ded39991506cf71f2246128e216c459d70f1f7
derrick <derrickmbarani@gmail.com>
2022-12-09 17:42:24 +0300

    Add Daos insert, insertMany and Delete fns

--- app/src/main/java/ke/derrick/androidroomwithpaging/data/RandomTextDao.kt
+++ app/src/main/java/ke/derrick/androidroomwithpaging/data/RandomTextDao.kt
@@ -2,10 +2,21 @@ package ke.derrick.androidroomwithpaging.data
 
 import androidx.paging.PagingSource
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
 
 @Dao
 interface RandomTextDao {
     @get:Query("SELECT * FROM random_text")
     val allRandomTexts: PagingSource<Int, RandomText>
+
+    @Insert
+    suspend fun insert(randomText: RandomText)
+
+    @Insert
+    suspend fun insertMany(vararg randomText: RandomText)
+
+    @Delete
+    suspend fun delete(randomText: RandomText)
 }
